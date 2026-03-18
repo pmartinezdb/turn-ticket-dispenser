3a711c90b3a18df68b91982ae1de0bfe84c0fc88
pablo.martinez <pablo.martinez@orquest.com>
2026-03-18 21:30:27 +0100

    Testing from different dispensers

--- src/test/java/com/orquest/TicketDispenserTest.java
+++ src/test/java/com/orquest/TicketDispenserTest.java
@@ -1,5 +1,6 @@
 package com.orquest;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,19 @@ class TicketDispenserTest {
 
         // Then
         assertTrue(ticket.getTurnNumber() >= 0);
+    }
+
+    @Test
+    void should_retrieve_two_different_tickets_from_two_dispensers() {
+        // Given
+        var dispenserOne = new TicketDispenser();
+        var dispenserTwo = new TicketDispenser();
+
+        // When
+        var ticketOne = dispenserOne.getTurnTicket();
+        var ticketTwo = dispenserTwo.getTurnTicket();
+
+        // Then
+        assertNotEquals(ticketOne.getTurnNumber(), ticketTwo.getTurnNumber());
     }
 }
